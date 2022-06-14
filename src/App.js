1413df91345b0c25f2ec95b3cd60aa17d01ef853
Leonardo Leonotti <leonardo.leonotti@conpec.com.br>
2022-06-13 23:39:25 -0300

    Game reset + ApiSettings

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import Intro from "./components/Intro";
 import Blob from "./components/Blob";
 import Question from "./components/Question";
+import blob1 from "./assets/shape-1.png";
+import blob2 from "./assets/shape-2.png";
 
 function App() {
     const [questions, setQuestions] = React.useState([]);
@@ -11,11 +13,13 @@ function App() {
     const [isGameStart, setIsGameStart] = React.useState(false);
     const [correctCounter, setCorrectCounter] = React.useState(0);
     const [disabled, setDisabled] = React.useState(false);
-    const [reset, setReset] = React.useState(false);
-
     const allQuestionsAnswered = questions.every(
         (question) => question.selectedAnswer !== ""
     );
+    const [apiSettings, setApiSettings] = React.useState({
+        category: 9,
+        difficulty: "",
+    });
 
     function shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
@@ -28,27 +32,32 @@ function App() {
         setIsGameStart((oldState) => !oldState);
     };
 
-    React.useEffect(function () {
-        fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-            .then((res) => res.json())
-            .then((data) =>
-                setQuestions(
-                    data.results.map((obj) => {
-                        return {
-                            selectedAnswer: "",
-                            isRight: false,
-                            id: nanoid(),
-                            answers: [
-                                ...obj.incorrect_answers,
-                                obj.correct_answer,
-                            ],
-                            ...obj,
-                        };
-                    })
-                )
-            );
-        // setQuestions((oldArray) => shuffleArray(oldArray));
-    }, []);
+    React.useEffect(
+        function () {
+            fetch(
+                `https://opentdb.com/api.php?amount=5&category=${apiSettings.category}&difficulty=${apiSettings.difficulty}&type=multiple`
+            )
+                .then((res) => res.json())
+                .then((data) =>
+                    setQuestions(
+                        data.results.map((obj) => {
+                            return {
+                                selectedAnswer: "",
+                                isRight: false,
+                                id: nanoid(),
+                                answers: [
+                                    ...obj.incorrect_answers,
+                                    obj.correct_answer,
+                                ],
+                                ...obj,
+                            };
+                        })
+                    )
+                );
+            // setQuestions((oldArray) => shuffleArray(oldArray));
+        },
+        [isGameStart]
+    );
 
     React.useEffect(() => {
         if (questions.length !== 0 && allQuestionsAnswered) {
@@ -60,7 +69,6 @@ function App() {
             });
 
             setCorrectCounter(correctAnswers);
-            // setCheckAnswerBtn(true);
         }
     }, [questions]);
 
@@ -95,6 +103,15 @@ function App() {
         questions.forEach((element) => (element.isRight ? (counter += 1) : 0));
         setCorrectCounter((oldValue) => oldValue + counter);
     };
+
+    const handleReset = () => {
+        setIsGameStart((oldState) => !oldState);
+        setQuestions([]);
+        setCorrectCounter(0);
+        setDisabled(false);
+        setIsGameOver(false);
+    };
+
     const componentsArray = questions.map((obj) => {
         return (
             <Question
@@ -114,12 +131,16 @@ function App() {
 
     return (
         <>
-            <Blob />
+            <img src={blob1} className="blob-top"></img>
             <main className="main-container">
                 {isGameStart ? (
                     componentsArray
                 ) : (
-                    <Intro startGame={startGame} />
+                    <Intro
+                        startGame={startGame}
+                        apiSettings={apiSettings}
+                        setApiSettings={setApiSettings}
+                    />
                 )}
             </main>
             {isGameStart && (
@@ -129,20 +150,18 @@ function App() {
                             ? `Você acertou ${correctCounter}/5 perguntas!`
                             : ""}
                     </p>
-                    {isGameOver ? (
-                        <button className="main-btn" onClick={setReset(true)}>
-                            {"Reset Game"}
-                        </button>
-                    ) : (
-                        <button
-                            className="main-btn"
-                            onClick={handleCheckAnswer}
-                        >
-                            {"Check answers"}
-                        </button>
-                    )}
+
+                    <button
+                        className="main-btn"
+                        onClick={() => {
+                            isGameOver ? handleReset() : handleCheckAnswer();
+                        }}
+                    >
+                        {isGameOver ? "Reset Game" : "Check answers"}
+                    </button>
                 </section>
             )}
+            <img src={blob2} className="blob-bottom"></img>
         </>
     );
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,10 +2,80 @@ import React from "react";
 import "./Intro.css";
 
 function Intro(props) {
+    function handleChange(event) {
+        console.log(event);
+        const { name, value, type, checked } = event.target;
+        props.setApiSettings((prevFormData) => {
+            return {
+                ...prevFormData,
+                [name]: type === "checkbox" ? checked : value,
+            };
+        });
+    }
+
     return (
         <div className="intro-container">
             <h1 className="title">Quizzical</h1>
-            <h2 className="description">Some description</h2>
+            <h2 className="description">
+                Choose a category and test your Knowledge!
+            </h2>
+            <div className="api-form-container">
+                <label htmlFor="categorySelector" className="api-form-label">
+                    Category
+                </label>
+                <select
+                    className="main-selector"
+                    name="category"
+                    id="categorySelector"
+                    value={props.apiSettings.category}
+                    onChange={handleChange}
+                >
+                    <option value="any">Any Category</option>
+                    <option value="9">General Knowledge</option>
+                    <option value="10">Entertainment: Books</option>
+                    <option value="11">Entertainment: Film</option>
+                    <option value="12">Entertainment: Music</option>
+                    <option value="13">Entertainment: Musicals Theatres</option>
+                    <option value="14">Entertainment: Television</option>
+                    <option value="15">Entertainment: Video Games</option>
+                    <option value="16">Entertainment: Board Games</option>
+                    <option value="17">Science &amp; Nature</option>
+                    <option value="18">Science: Computers</option>
+                    <option value="19">Science: Mathematics</option>
+                    <option value="20">Mythology</option>
+                    <option value="21">Sports</option>
+                    <option value="22">Geography</option>
+                    <option value="23">History</option>
+                    <option value="24">Politics</option>
+                    <option value="25">Art</option>
+                    <option value="26">Celebrities</option>
+                    <option value="27">Animals</option>
+                    <option value="28">Vehicles</option>
+                    <option value="29">Entertainment: Comics</option>
+                    <option value="30">Science: Gadgets</option>
+                    <option value="31">
+                        Entertainment: Japanese Anime &amp; Manga
+                    </option>
+                    <option value="32">
+                        Entertainment: Cartoon &amp; Animations
+                    </option>{" "}
+                </select>
+                <label htmlFor="difficultySelector" className="api-form-label">
+                    Difficulty
+                </label>
+                <select
+                    className="main-selector"
+                    name="difficulty"
+                    id="difficultySelector"
+                    value={props.apiSettings.difficulty}
+                    onChange={handleChange}
+                >
+                    <option value="any">Any Difficulty</option>
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="hard">Hard</option>
+                </select>
+            </div>
             <button className="main-btn" onClick={props.startGame}>
                 Start Quiz
             </button>
